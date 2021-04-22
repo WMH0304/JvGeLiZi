001243e4dedb1c1403b21a10a6f3e8cfafe3bdfb
王明海 <tk-7@foxmeail.com>
2021-04-22 23:11:10 +0800

    WPF动画

---
+++ WPFUI_test/Test/StoryBoardTest.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace WPFUI_test.Test
+{
+    /// <summary>
+    /// StoryBoardTest.xaml 的交互逻辑
+    /// </summary>
+    public partial class StoryBoardTest : Window
+    {
+        public StoryBoardTest()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 检索时间更改进度条
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Storyboard_CurrentTimeInvalidated(object sender, EventArgs e)
+        {
+            Clock clock = (Clock)sender;
+            if (clock == null)
+                this.pg.Value = 0;
+            else
+                this.pg.Value = (double)clock.CurrentProgress;
+        }
+    }
+}
---
+++ net_页面传值方法/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace net_页面传值方法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // string str = Request.
+
+        }
+    }
+}
